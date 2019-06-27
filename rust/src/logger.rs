77de2a0892556643409936b8b55c5e723178f9f5
aston <astonbitecode@gmail.com>
2019-06-27 10:40:50 +0300

    Defaulting J4RS_CONSOLE_LOG_LEVEL to warning.

--- rust/src/logger.rs
+++ rust/src/logger.rs
@@ -15,7 +15,7 @@ use std::env;
 
 lazy_static! {
     static ref CONSOLE_ENABLED: i8 = {
-      let var_level = env::var("J4RS_CONSOLE_LOG_LEVEL").unwrap_or("info".to_owned()).to_lowercase();
+      let var_level = env::var("J4RS_CONSOLE_LOG_LEVEL").unwrap_or("warn".to_owned()).to_lowercase();
       match var_level.as_str() {
         "disabled" => 0,
         "error" => 1,
@@ -23,8 +23,8 @@ lazy_static! {
         "info" => 3,
         "debug" => 4,
         _ => {
-          println!("WARN: The env variable 'J4RS_CONSOLE_LOG_LEVEL' is not correctly set. Please use one of the 'debug', 'info', 'warn', 'error', or 'disabled'. Defaulting to info");
-          3
+          println!("WARN: The env variable 'J4RS_CONSOLE_LOG_LEVEL' is not correctly set. Please use one of the 'debug', 'info', 'warn', 'error', or 'disabled'. Defaulting to warning");
+          2
         }
       }
     };
