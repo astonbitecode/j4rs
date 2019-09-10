c606b27fb9a297e9256184948ca82cc05700953f
aston <astonbitecode@gmail.com>
2019-09-10 09:13:40 +0300

    Fixed copying j4rs jar to the jassets directory when needed.

--- rust/build.rs
+++ rust/build.rs
@@ -86,7 +86,7 @@ fn copy_jars_from_java(jar_source_path: &str) -> Result<(), J4rsBuildError> {
         // Copy only if the files are not the same
         let do_copy = if destination_jar_file_res.is_ok() {
             let mut destination_jar_file = destination_jar_file_res.unwrap();
-            are_same_files(&mut source_jar_file, &mut destination_jar_file).unwrap_or(true)
+            !are_same_files(&mut source_jar_file, &mut destination_jar_file).unwrap_or(true)
         } else { true };
 
         if do_copy {
