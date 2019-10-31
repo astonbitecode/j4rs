47ddab6db4d82e9b7cc7a64565575859d15b4cd9
aston <astonbitecode@gmail.com>
2019-10-31 16:40:03 +0200

    #12: Delete unnecessairy casting while creating collections.

--- java/src/main/java/org/astonbitecode/j4rs/api/instantiation/NativeInstantiationImpl.java
+++ java/src/main/java/org/astonbitecode/j4rs/api/instantiation/NativeInstantiationImpl.java
@@ -100,17 +100,15 @@ public class NativeInstantiationImpl {
             Array.set(arrayObj, i, paramObjects[i]);
         }
 
-        Object[] oo = (Object[]) arrayObj;
-
         switch (collectionType) {
             case Array:
-                return new CreatedInstance(oo.getClass(), oo);
+                return new CreatedInstance(arrayObj.getClass(), arrayObj);
             case List: {
-                List<?> l = Arrays.asList(oo);
+                List<?> l = Arrays.asList(arrayObj);
                 return new CreatedInstance(l.getClass(), l);
             }
             default:
-                return new CreatedInstance(oo.getClass(), oo);
+                return new CreatedInstance(arrayObj.getClass(), arrayObj);
         }
 
     }
