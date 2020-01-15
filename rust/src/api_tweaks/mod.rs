76af85c162ec4f809aca6eff82fb2b39b783ef13
aston <astonbitecode@gmail.com>
2020-01-15 15:14:50 +0200

    #13: Give it a shot: Delete macos specific code.

--- rust/src/api_tweaks/mod.rs
+++ rust/src/api_tweaks/mod.rs
@@ -15,25 +15,25 @@ use std::os::raw::c_void;
 // limitations under the License.
 use jni_sys::{JavaVM, jclass, jint, JNIEnv, jsize};
 
-#[cfg(not(any(target_os = "android", target_os = "macos")))]
+#[cfg(not(any(target_os = "android")))]
 mod generic;
 
-#[cfg(not(any(target_os = "android", target_os = "macos")))]
+#[cfg(not(any(target_os = "android")))]
 pub fn get_created_java_vms(vm_buf: &mut Vec<*mut JavaVM>, buf_len: jsize, n_vms: *mut jsize) -> jint {
     generic::get_created_java_vms(vm_buf, buf_len, n_vms)
 }
 
-#[cfg(not(any(target_os = "android", target_os = "macos")))]
+#[cfg(not(any(target_os = "android")))]
 pub fn set_java_vm(_: *mut JavaVM) {}
 
-#[cfg(not(any(target_os = "android", target_os = "macos")))]
+#[cfg(not(any(target_os = "android")))]
 pub fn create_java_vm(
     pvm: *mut *mut JavaVM,
     penv: *mut *mut c_void,
     args: *mut c_void,
 ) -> jint { generic::create_java_vm(pvm, penv, args) }
 
-#[cfg(not(any(target_os = "android", target_os = "macos")))]
+#[cfg(not(any(target_os = "android")))]
 pub fn find_class(env: *mut JNIEnv, classname: &str) -> jclass {
     generic::find_class(env, classname)
 }
@@ -63,29 +63,4 @@ pub fn create_java_vm(
 #[cfg(target_os = "android")]
 pub fn find_class(env: *mut JNIEnv, classname: &str) -> jclass {
     android::find_class(env, classname)
-}
-
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
-
-#[cfg(target_os = "macos")]
-mod macos;
-
-#[cfg(target_os = "macos")]
-pub fn get_created_java_vms(vm_buf: &mut Vec<*mut JavaVM>, buf_len: jsize, n_vms: *mut jsize) -> jint {
-    macos::get_created_java_vms(vm_buf, buf_len, n_vms)
-}
-
-#[cfg(target_os = "macos")]
-pub fn set_java_vm(_: *mut JavaVM) {}
-
-#[cfg(target_os = "macos")]
-pub fn create_java_vm(
-    pvm: *mut *mut JavaVM,
-    penv: *mut *mut c_void,
-    args: *mut c_void,
-) -> jint { macos::create_java_vm(pvm, penv, args) }
-
-#[cfg(target_os = "macos")]
-pub fn find_class(env: *mut JNIEnv, classname: &str) -> jclass {
-    macos::find_class(env, classname)
 }
