40961df5f98ceddeb909002cbda99255bce65eae
aston <astonbitecode@gmail.com>
2020-03-12 10:40:22 +0200

    Bump v0.11.2

--- rust/build.rs
+++ rust/build.rs
@@ -25,7 +25,7 @@ use std::path::{Path, PathBuf};
 use java_locator;
 use sha2::{Digest, Sha256};
 
-const VERSION: &'static str = "0.11.1-java7";
+const VERSION: &'static str = "0.11.2-java7";
 
 fn main() -> Result<(), J4rsBuildError> {
     let out_dir = env::var("OUT_DIR")?;
