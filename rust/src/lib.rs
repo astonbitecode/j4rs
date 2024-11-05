06546711954e8265a6040a6a3d0a6d91c0e1ce4b
aston <astonbitecode@gmail.com>
2024-11-05 17:32:38 +0200

    v0.21.0

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -15,7 +15,7 @@
 //! # j4rs
 //!
 //![![crates.io](https://img.shields.io/crates/v/j4rs.svg)](https://crates.io/crates/j4rs)
-//![![Maven Central](https://img.shields.io/badge/Maven%20Central-0.20.0-blue.svg)](https://central.sonatype.com/artifact/io.github.astonbitecode/j4rs/)
+//![![Maven Central](https://img.shields.io/badge/Maven%20Central-0.21.0-blue.svg)](https://central.sonatype.com/artifact/io.github.astonbitecode/j4rs/)
 //!![Build](https://github.com/astonbitecode/j4rs/actions/workflows/ci-workflow.yml/badge.svg)
 //!
 //!j4rs stands for __'Java for Rust'__ and allows effortless calls to Java code from Rust and vice-versa.
